22d7afb48fa5b11add814020f499fc44e8372438
Thevetat <thevetat@proton.me>
2025-08-14 19:06:32 -0500

    Fix which-key syntax for leader+s save mapping

--- .config/nvim/lua/plugins/save-keymaps.lua
+++ .config/nvim/lua/plugins/save-keymaps.lua
@@ -1,25 +1,10 @@
 return {
   {
     "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      spec = {
-        { "<leader>s", group = false },
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    init = function()
-      -- Override search keymaps with save
-      vim.api.nvim_create_autocmd("VeryLazy", {
-        callback = function()
-          -- Remove the search keymap group if it exists
-          pcall(vim.keymap.del, "n", "<leader>s")
-          
-          -- Add save keybinding
-          vim.keymap.set({ "n", "i", "v" }, "<leader>s", "<cmd>w<cr><esc>", { desc = "Save file", silent = true })
-        end,
+    opts = function(_, opts)
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>s", "<cmd>w<cr>", desc = "Save file", mode = { "n", "i", "v" } },
       })
     end,
   },
