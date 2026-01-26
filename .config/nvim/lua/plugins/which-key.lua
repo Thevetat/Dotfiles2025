3b8a55ef6cf5cdfb0b5f792fe1016a340592f7a8
Thevetat <thevetat@proton.me>
2026-01-25 21:06:37 -0600

    Fix nvim which-key mapping hijacking insert mode - remove i mode from leader+s save binding, add super-tab completion preset

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
     "folke/which-key.nvim",
     opts = {
       spec = {
-        { "<leader>s", "<cmd>w<cr>", desc = "Save file", mode = { "n", "i", "v" } },
+        { "<leader>s", "<cmd>w<cr>", desc = "Save file", mode = { "n", "v" } },
         { "<leader>C", group = "code" },
         { "<leader>c", group = "claude" },
       },
