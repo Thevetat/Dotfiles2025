84b56283532bc8313d1fb038b4ff1314541c6994
Thevetat <thevetat@proton.me>
2026-08-25 02:37:26 -0500

    Refine Noctalia appearance controls

---
+++ .config/hypr/config/colors.lua
@@ -0,0 +1,11 @@
+-- Cachy colors
+
+CACHYLGREEN = "rgba(82dcccd9)"
+CACHYMGREEN = "rgba(00aa84d9)"
+CACHYDGREEN = "rgba(007d6fd9)"
+CACHYLBLUE  = "rgba(01ccffff)"
+CACHYMBLUE  = "rgba(182545ff)"
+CACHYDBLUE  = "rgba(111826ff)"
+CACHYWHITE  = "rgba(ffffffff)"
+CACHYGREY   = "rgba(ddddddff)"
+CACHYGRAY   = "rgba(798bb2ff)"
