b30e69b01dc50c07368a7cf074ebe16654a46570
Thevetat <thevetat@proton.me>
2026-08-26 15:14:31 -0500

    Remove gaming workspace

--- .config/hypr/config/workspaces.lua
+++ .config/hypr/config/workspaces.lua
@@ -1,6 +1,4 @@
 -- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
-hl.workspace_rule({ workspace = "name:gaming", monitor = PRIMARY_MONITOR, default = true })
-
 for workspace = 1, 5 do
   hl.workspace_rule({
     workspace = tostring(workspace),
